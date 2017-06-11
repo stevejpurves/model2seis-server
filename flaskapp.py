32509214ae940aaa7f552903064172e4bda71936
Jesper Dramsch <jesper@dramsch.net>
2017-06-11 06:38:15 +0000

    Forward API

--- flaskapp.py
+++ flaskapp.py
@@ -3,7 +3,13 @@ import os
 sys.path.append(os.path.abspath('./src'))
 from flask import Flask, request, send_from_directory, jsonify
 import json
+from binascii import a2b_base64
+import time,hashlib
 import numpy as np
+import urllib, cStringIO
+from tempfile import NamedTemporaryFile
+from src import sketch2model
+
 app = Flask(__name__, static_url_path='')
 
 
@@ -14,9 +20,22 @@ def get_model(id):
 
 @app.route('/api/forward', methods=['POST'])
 def get_forward_model():
-  seismic= model2seismic()
-  return jsonify({"seismic": list(seismic)})
-  
+  #return jsonify({"ok": True}) 
+  #tempFileObj = NamedTemporaryFile(mode='w+b',suffix='jpg')
+  data = request.get_json(force=True)
+  #binary_data = a2b_base64(data['dataUrl'])
+  hash = hashlib.sha1()
+  hash.update(str(time.time()))
+  name=hash.hexdigest()[:10]
+  fd = open('./tmp/'+name,'wb')
+  #fd.write(binary_data)
+  fd.close
+  r = sketch2model.call_sketch2model(url="http://ec2-35-158-38-106.eu-central-1.compute.amazonaws.com/tmp/"+name)
+  #tempFileObj = cStringIO.StringIO(urllib.urlopen(r['url']).read())  
+  #tempFileObj.seek(0,0)
+  #response = send_file(tempFileObj, attachment_filename='myfile.png')
+  return data['dataUrl']
+ 
 @app.route('/api/inverse')
 def get_inverse_model():
   model = seismic2model()
@@ -24,7 +43,8 @@ def get_inverse_model():
 
 @app.route('/api/heartbeat')
 def pong():
-  return jsonify({"ReturnCode": 200})
+  return jsonify({"ok": True})
+
 
 @app.route('/')
 def root():
---
+++ src/sketch2model.py
@@ -0,0 +1,30 @@
+import requests
+from tempfile import NamedTemporaryFile
+from shutil import copyfileobj
+from os import remove
+
+def call_sketch2model(url = "https://s3-us-west-2.amazonaws.com/sketch2model/examples/Simple.png", \
+  contrast = "0.5", closing = "3",cmap = "gray"):
+  payload = {
+    "url": url,
+    "contrast": contrast,
+    "closing": closing,
+    "cmap": cmap
+  }
+
+  headers = {"Accept": "application/json"}
+
+  r = requests.get("http://www.sketch2model.com/api",headers=headers,params=payload)
+  return r.json()
+
+def serve_model():
+  tempFileObj = NamedTemporaryFile(mode='w+b',suffix='jpg')
+  pilImage = open('/tmp/myfile.jpg','rb')
+  copyfileobj(pilImage,tempFileObj)
+  pilImage.close()
+  remove('/tmp/myfile.jpg')
+  tempFileObj.seek(0,0)
+
+if __name__ == "__main__":
+  print call_sketch2model()
+
