02ea735f1890e773d73e6c82e87dee41c446d444
Jesper Dramsch <jesper@dramsch.net>
2017-06-11 08:44:20 +0000

    Model fix

--- flaskapp.py
+++ flaskapp.py
@@ -9,13 +9,13 @@ import urllib, cStringIO
 from tempfile import NamedTemporaryFile
 from src import sketch2model
 
-app = Flask(__name__, static_url_path='')
-
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = "/tmp"
 
 @app.route('/api/model/<id>')
 def get_model(id):
   filename = str(id) + '.png';
-  return app.send_static_file('tmp/' + filename)
+  return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
 
 @app.route('/api/forward', methods=['POST'])
 def get_forward_model():
@@ -27,14 +27,14 @@ def get_forward_model():
   hash = hashlib.sha1()
   hash.update(str(time.time()))
   name=hash.hexdigest()[:10]
-  fd = open('./tmp/'+name+'.png','wb')
+  fd = open(app.config['UPLOAD_FOLDER']+'/'+name+'.png','wb')
   fd.write(binary_data)
   fd.close
-  r = sketch2model.call_sketch2model(url="http://ec2-35-158-38-106.eu-central-1.compute.amazonaws.com/tmp/"+name)
+  #r = sketch2model.call_sketch2model(url="http://ec2-35-158-38-106.eu-central-1.compute.amazonaws.com/model/"+name)
   #tempFileObj = cStringIO.StringIO(urllib.urlopen(r['url']).read())  
   #tempFileObj.seek(0,0)
   #response = send_file(tempFileObj, attachment_filename='myfile.png')
-  return jsonify({ "modelUrl": '/api/model/'+name, "seismicUrl": '/api/model/'+name })
+  return jsonify({ "modelUrl": '/api/model/'+name, "seismicUrl": ""})
 
 @app.route('/api/forward-model_only')
 def get_forward_from_model_only():
